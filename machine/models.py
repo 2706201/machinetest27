c28d6657093d871fbeae487739f78b0a5beae9d5
Priti Vikas More <116071471+2706201@users.noreply.github.com>
2024-03-22 20:08:32 +0530

    Update models.py

--- machine/models.py
+++ machine/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    client_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Add any additional fields here as needed
+
+    def __str__(self):
+        return self.client_name
