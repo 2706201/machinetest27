871675a96decb9e7e310abb7da397e296227a04f
Priti Vikas More <116071471+2706201@users.noreply.github.com>
2024-04-03 15:03:08 +0530

    Update views.py

--- machine/views.py
+++ machine/views.py
@@ -1,36 +1,62 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from api.models import *
-from api.serializers import *
-from rest_framework.decorators import action
-from rest_framework.response import Response
-
-
-# Create your views here.
-
-class ClientViewSet(viewsets.ModelViewSet):
-    queryset= Client.objects.all()
-    serializer_class=ClientSerializer
-    
-
-    @action(detail=True,methods=['get'])
-    def projects(self,request,pk=None):   
-        try:                
-            client=Client.objects.get(pk=pk)
-            proj=Project.objects.filter(client=client)
-            proj_serializer = ProjectSerializer(proj,many=True)
-            return Response(proj_serializer.data)
-        except Exception as e:
-            print(e)
-            return Response({
-                'message':'Client might not exists !! Error'
-            })
-
-
-class ProjectViewSet(viewsets.ModelViewSet):
-    queryset=Project.objects.all()
-    serializer_class=ProjectSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
+from django.shortcuts import render 
+from rest_framework import generics 
+from .serializer import ClientSerializer,ProjectSerializer 
+from rest_framework.views import APIView 
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response 
+from .models import * 
+from rest_framework import viewsets 
+  
+ # Register a client 
+class addClient(generics.CreateAPIView): 
+     serializer_class=ClientSerializer 
+     queryset=Client.objects.all() 
+  
+ #List a clients 
+class listClient(generics.ListAPIView): 
+     queryset=Client.objects.all() 
+     serializer_class=ClientSerializer 
+  
+ #Edit client info 
+class editClient(generics.RetrieveUpdateAPIView): 
+     queryset=Client.objects.all() 
+     serializer_class=ClientSerializer 
+  
+ #Delete client Info 
+class deleteClient(generics.DestroyAPIView): 
+     queryset=Client.objects.all() 
+     serializer_class=ClientSerializer 
+  
+ #Add new projects for a client and assign one or multiple users to those projects. 
+class createListProject(generics.ListCreateAPIView): 
+     queryset=Project.objects.all() 
+     serializer_class=ProjectSerializer 
+  
+ #Update or Delete project and its user or clients 
+class editDeleteProject(generics.RetrieveUpdateDestroyAPIView): 
+     queryset=Project.objects.all() 
+     serializer_class=ProjectSerializer 
+  
+ #Index Page 
+def index(request): 
+     return render(request,'index.html') 
+  
+ # List of all projects assigned to the user 
+from rest_framework import status 
+from rest_framework.response import Response 
+@api_view(['GET']) 
+def getProject(request,id): 
+     try: 
+         obj=Project.objects.filter(users = id) 
+     except obj.DoesNotExist: 
+         return Response(status=status.HTTP_404_NOT_FOUND) 
+     queryset = Project.objects.filter(users=id) 
+     serializer = ProjectSerializer(obj,many=True) 
+     serializer_class = ProjectSerializer 
+  
+     print( obj) 
+     return Response(serializer.data) 
+  
+class anoyomous(viewsets.ModelViewSet): 
+     queryset=Project.objects.all() 
+     serializer_class=ProjectSerializer
