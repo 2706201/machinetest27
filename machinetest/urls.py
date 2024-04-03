42f9709750e3f48bd730f5ad2e233f05d6432eac
Priti Vikas More <116071471+2706201@users.noreply.github.com>
2024-04-03 15:08:17 +0530

    Update urls.py

--- machinetest/urls.py
+++ machinetest/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for machinetest project.
+"""nimapapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,23 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from django.views.generic import RedirectView
-from .views import ClientListCreateView, ClientRetrieveUpdateDestroyView, ProjectListCreateView, ProjectRetrieveUpdateDestroyView
+from django.contrib import admin
+from django.urls import path,include
+from machine import views as v
+from rest_framework import routers
+router=routers.DefaultRouter()
+router.register('anoymous',v.anoymous)
+
 
 urlpatterns = [
-    # Redirect empty path to 'clients/'
-    path('', RedirectView.as_view(url='clients/', permanent=False)),
-
-    # Include your app's URL patterns
-    path('clients/', include([
-        # Clients URLs
-        path('', ClientListCreateView.as_view(), name='client-list-create'),
-        path('<int:pk>/', ClientRetrieveUpdateDestroyView.as_view(), name='client-retrieve-update-destroy'),
-
-        # Projects URLs
-        path('<int:pk>/projects/', ProjectListCreateView.as_view(), name='project-list-create'),
-        path('projects/<int:pk>/', ProjectRetrieveUpdateDestroyView.as_view(), name='project-retrieve-update-destroy'),
-    ])),
+    path('admin/', admin.site.urls),
+    path('api_auth/',include('rest_framwork.urls')),
+    path('',include('machine.urls')),
+    path('link',include(router.urls)),
 ]
-
