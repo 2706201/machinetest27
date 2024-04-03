aa1445662384903e4519bf35a65339169a2aad56
Priti Vikas More <116071471+2706201@users.noreply.github.com>
2024-04-03 15:02:03 +0530

    Update urls.py

--- machine/urls.py
+++ machine/urls.py
@@ -1,36 +1,14 @@
-"""
-URL configuration for machinetest project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path, include
-from django.views.generic import RedirectView
-from .views import ClientListCreateView, ClientRetrieveUpdateDestroyView, ProjectListCreateView, ProjectRetrieveUpdateDestroyView
-
-urlpatterns = [
-    # Redirect empty path to 'clients/'
-    path('', RedirectView.as_view(url='clients/', permanent=False)),
-
-    # Include your app's URL patterns
-    path('clients/', include([
-        # Clients URLs
-        path('', ClientListCreateView.as_view(), name='client-list-create'),
-        path('<int:pk>/', ClientRetrieveUpdateDestroyView.as_view(), name='client-retrieve-update-destroy'),
-
-        # Projects URLs
-        path('<int:pk>/projects/', ProjectListCreateView.as_view(), name='project-list-create'),
-        path('projects/<int:pk>/', ProjectRetrieveUpdateDestroyView.as_view(), name='project-retrieve-update-destroy'),
-    ])),
-]
-
+from django.contrib import admin 
+from django.urls import path,include 
+from . import views as v 
+  
+urlpatterns = [ 
+     path('',v.index), 
+     path('POST/client',v.addClient.as_view()), 
+     path('GET/client',v.listClient.as_view()), 
+     path('PUT-PATCH/client/:<int:pk>',v.editClient.as_view()), 
+     path('DELETE/clients/:<int:pk>',v.deleteClient.as_view()), 
+     path('GET-POST/projects',v.createListProject.as_view()), 
+     path('editDeleteProject/:<int:pk>',v.editDeleteProject.as_view()), 
+     path('listProject/:<int:id>',v.getProject), 
+ ]
